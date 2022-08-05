d65958905bea01bf63929a8a6c21b55772f382df
nmhung1234 <hung.nm.here@gmail.com>
2022-08-05 23:54:39 +0700

    add base blog, galery

--- element/home/Blog/index.tsx
+++ element/home/Blog/index.tsx
@@ -1,5 +1,31 @@
+import { Image } from "@mantine/core";
+import style from "./style.module.css";
+
 const Blog = () => {
-    return <div>Blog</div>;
+  return (
+    <div className="flex flex-wrap max-w-[1200px] mx-auto">
+      <BlogItem />
+      <BlogItem />
+      <BlogItem />
+    </div>
+  );
+};
+
+const BlogItem = () => {
+  return (
+    <div
+      className={`w-full mx-auto max-w-[400px] m-4 lg:w-[calc(33%_-_32px)] rounded-md relative text-ct-neutral-01 overflow-hidden ${style.dad}`}
+    >
+      <Image src="/images/blog-1.jpg" width="100%" height="400" alt="blog" />
+      <div
+        className={`transition-all duration-500 ease-in-out absolute flex flex-col justify-end p-6 w-full h-full top-0 bg-gradient-to-b from-transparent to-[#000000de] ${style.child}`}
+      >
+        <p className="bg-ct-secondary-02 rounded-lg p-1 px-3 w-fit">Blog</p>
+        <p className="text-2xl my-4">Lorem ipsum dolor sit amet, consect</p>
+        <p className="text-sm font-thin">12 tháng 6, 2022 • ADMIN</p>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- element/home/Galery/index.tsx
+++ element/home/Galery/index.tsx
@@ -1,5 +1,56 @@
+import { Image } from "@mantine/core";
 const Galery = () => {
-    return <div>Galery</div>;
+  return (
+    <div className="max-w-[1200px] mx-auto flex flex-wrap">
+      <div className="w-1/2 grow p-4">
+        <Image
+          className="rounded-sm overflow-hidden"
+          src="/images/gallery1.jpg"
+          width="100%"
+          height="100%"
+          alt="galery"
+        />
+      </div>
+      <div className="min-w-[450px] grow w-1/2 flex flex-wrap justify-between">
+        <div className="p-4 w-1/2 h-1/2 ">
+          <Image
+            className="w-full h-full rounded-sm overflow-hidden"
+            src="/images/gallery1.jpg"
+            width="100%"
+            height="100%"
+            alt="galery"
+          />
+        </div>
+        <div className="p-4 w-1/2 h-1/2 ">
+          <Image
+            className="w-full h-full rounded-sm overflow-hidden"
+            src="/images/gallery1.jpg"
+            width="100%"
+            height="100%"
+            alt="galery"
+          />
+        </div>
+        <div className="p-4 w-1/2 h-1/2 ">
+          <Image
+            className="w-full h-full rounded-sm overflow-hidden"
+            src="/images/gallery1.jpg"
+            width="100%"
+            height="100%"
+            alt="galery"
+          />
+        </div>
+        <div className="p-4 w-1/2 h-1/2 ">
+          <Image
+            className="w-full h-full rounded-sm overflow-hidden"
+            src="/images/gallery1.jpg"
+            width="100%"
+            height="100%"
+            alt="galery"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Galery;
