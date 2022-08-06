2e0cb022f14da37cdf1be8e29dc8e97095e1f1a5
xuanchien0111 <chiennx@hocmai.vn>
2022-08-06 14:09:46 +0700

    fix syntax error

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,80 +4,78 @@ import React from "react";
 import style from "./style.module.css";
 
 const Header = () => {
-    const styleText = 'flex items-center mr-10'
-    return <div className="">
-       <div className={`w-full flex justify-between items-center p-7 `}>
-            <div className="">
-                <Image
-                    src="http://demo2.themelexus.com/kindero/wp-content/uploads/2019/12/logo.svg"
-                    alt="Random unsplash image"
-                    width={100}
-                />
-            </div>
-            <div className="mx-26 md:block hidden">
-                    <nav className={ `${style.nav}`}>
-                        <ul className="list-none">
-                            <li className={ `${style.dropdown} `}><a href="" className="flex items-center mr-4">Home <ArrowDown2 size="12" className="ml-2"/></a>
-                                <ul className={ `${style.dropdownnav}`}>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Home1</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Home2</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Home3</a></li>
-                                </ul>
-                            </li>
-                            <li className={ `${style.dropdown} `}><a href="/curriculum" className="flex items-center mr-4">Curriculum <ArrowDown2 size="12" className="ml-2"/></a>
-                                <ul className={ `${style.dropdownnav}`}>
-                                    <li><a href="/curriculum"><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>All Curriculum</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Single curriculum</a></li>
-                                </ul>
-                            </li>
-                            <li className={ `${style.dropdown} `}><a href="" className="flex items-center mr-4">Classes <ArrowDown2 size="12" className="ml-2"/></a>
-                                <ul className={ `${style.dropdownnav}`}>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>All Classes</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Classes-tabs</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Classes – Single</a></li>
-                                </ul>
-                            </li>
-                            <li className={ `${style.dropdown} `}><a href="/about-us" className="flex items-center mr-4">About Us <ArrowDown2 size="12" className="ml-2"/></a>
-                                <ul className={ `${style.dropdownnav}`}>
-                                    <li><a href="/about-us"><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>About us</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Principals message</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>About facilities</a></li>
-                                </ul>
-                            </li>
-                            <li className={ `${style.dropdown} `}><a href="" className="flex items-center mr-4">Blog <ArrowDown2 size="12" className="ml-2"/></a>
-                                <ul className={ `${style.dropdownnav}`}>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Blog – Listing</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Single Post</a></li>
-                                </ul>
-                            </li>
-                            <li className={ `${style.dropdown} `}><a href="" className="flex items-center mr-4">Page <ArrowDown2 size="12" className="ml-2"/></a>
-                                <ul className={ `${style.dropdownnav}`}>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Contact Us</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Events List</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Events Grid</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Faq</a></li>
-                                    <li><a href=""><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Page 404</a></li>
-                                </ul>
-                            </li>
-                            
-                        </ul>
-                    </nav>
-            </div>
-            <div>
-                <div>
-                    <div className="lg:block hidden">
-                        <a href="">
-                            <p> Have any questions? </p>
-                            <p className="font-semibold text-ct-secondary-02">  +844 1800 33355  </p>
-                        </a>
-                    </div>
-                    <div className="md:hidden block">
-                        <HambergerMenu size="32" />
-                    </div>
-                </div>
-            </div>
-       </div>
-    </div>;
+    return <div >
+        <div className={`w-full flex justify-between items-center p-7 `}>
+         <div className="">
+             <Image
+                 src="http://demo2.themelexus.com/kindero/wp-content/uploads/2019/12/logo.svg"
+                 alt="Random unsplash image"
+                 width={100}
+             />
+         </div>
+         <div className="mx-26 md:block hidden">
+                 <nav className={ `${style.nav}`}>
+                     <ul className="list-none">
+                         <li className={ `${style.dropdown} `}><span className="flex items-center mr-4">Home <ArrowDown2 size="12" className="ml-2"/></span>
+                             <ul className={ `${style.dropdownnav}`}>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Home1</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Home2</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Home3</li>
+                             </ul>
+                         </li>
+                         <li className={ `${style.dropdown} `}><span className="flex items-center mr-4">Curriculum <ArrowDown2 size="12" className="ml-2"/></span>
+                             <ul className={ `${style.dropdownnav}`}>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>All Curriculum</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Single curriculum</li>
+                             </ul>
+                         </li>
+                         <li className={ `${style.dropdown} `}><span className="flex items-center mr-4">Classes <ArrowDown2 size="12" className="ml-2"/></span>
+                             <ul className={ `${style.dropdownnav}`}>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>All Classes</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Classes-tabs</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Classes – Single</li>
+                             </ul>
+                         </li>
+                         <li className={ `${style.dropdown} `}><span className="flex items-center mr-4">About Us <ArrowDown2 size="12" className="ml-2"/></span>
+                             <ul className={ `${style.dropdownnav}`}>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>About us</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Principals message</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>About facilities</li>
+                             </ul>
+                         </li>
+                         <li className={ `${style.dropdown} `}><span className="flex items-center mr-4">Blog <ArrowDown2 size="12" className="ml-2"/></span>
+                             <ul className={ `${style.dropdownnav}`}>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Blog – Listing</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Single Post</li>
+                             </ul>
+                         </li>
+                         <li className={ `${style.dropdown} `}><span className="flex items-center mr-4">Page <ArrowDown2 size="12" className="ml-2"/></span>
+                             <ul className={ `${style.dropdownnav}`}>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Contact Us</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Events List</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Events Grid</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Faq</li>
+                                 <li><ArrowCircleDown size="12" className={`${style.icon} mr-2 hidden`}/>Page 404</li>
+                             </ul>
+                         </li>
+                         
+                     </ul>
+                 </nav>
+         </div>
+         <div>
+             <div>
+                 <div className="lg:block hidden">
+                         <p> Have any questions? </p>
+                         <p className="font-semibold text-ct-secondary-02">  +844 1800 33355  </p>
+                     
+                 </div>
+                 <div className="md:hidden block">
+                     <HambergerMenu size="32" />
+                 </div>
+             </div>
+         </div>
+    </div>
+ </div>;
 };
 
 export default Header;
