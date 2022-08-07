ee663b37a1aa7a5a2de57cb7f36b7740b81ea885
nmhung1234 <hung.nm.here@gmail.com>
2022-08-07 10:29:52 +0700

    add map

--- element/contactUs/map/index.tsx
+++ element/contactUs/map/index.tsx
@@ -1,22 +1,12 @@
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import dynamic from "next/dynamic";
+const Map = dynamic(() => import("../../../components/Map"), { ssr: false });
 
 const Mapcontact = () => {
-    const [viewport, setViewport] = useState({
-        width: "100%",
-        height: "200hv",
-        latitude: 21.0244246,
-        longitude: 105.7938072,
-        zoom: 12
-    })
-    return <div>
-        <ReactMapGL
-        {...viewport}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        // onViewportChange={(viewport => setViewport(viewport))}
-        mapboxAccessToken="pk.eyJ1IjoiY2hpZW5ueCIsImEiOiJjbDZoenJnZ3EwcGM1M2pycGZqMXh6NG45In0.fkuhfQ-SljRS1o2dXenQhg"
-        />
-    </div>;
+  return (
+    <div className="w-full h-[500px] p-10 max-w-[1200px] mx-auto">
+      <Map />
+    </div>
+  );
 };
 
 export default Mapcontact;
