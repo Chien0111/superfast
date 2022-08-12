1656913e85ebfa0d52c7824f89a0cb3d10e13175
nmhung1234 <nmhung1234@hotmail.com>
2022-08-12 09:52:06 +0700

    validate form

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,22 +1,19 @@
-import { Divider, Image, PasswordInput, TextInput } from "@mantine/core";
-import React from "react";
 import AuthLayout from "../../layout/auth";
 
-import * as Yup from "yup";
+import { Divider, Image, PasswordInput, TextInput } from "@mantine/core";
 import { useForm, yupResolver } from "@mantine/form";
 import { Eye, EyeSlash } from "iconsax-react";
 import Link from "next/link";
+import * as Yup from "yup";
 
 const SignIn = () => {
-  const phoneRegExp = /(84|0[3|5|7|8|9])+([0-9]{8})\b/g;
+  const mailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   const schema = Yup.object().shape({
     email: Yup.string()
-      .required("Vui lòng nhập tên phụ huynh")
-      .min(2, "Vui lòng nhập đầy đủ phụ huynh")
-      .trim(),
-    password: Yup.string()
-      .matches(phoneRegExp, "Vui lòng nhập số điện thoại đúng quy định")
-      .required("Vui lòng nhập số điện thoại"),
+      .trim()
+      .matches(mailRegExp, "Vui lòng nhập email chính xác")
+      .required("Username / Email không được để trống"),
+    password: Yup.string().required("Mật khẩu không được để trống").trim(),
   });
   const signInForm = useForm({
     initialValues: {
@@ -25,6 +22,10 @@ const SignIn = () => {
     },
     validate: yupResolver(schema),
   });
+  const handleSubmit = (value: any) => {
+    console.log(value);
+    // signInForm.setFieldError("email", "Invalid email");
+  };
   return (
     <div className="w-[480px] mx-auto self-start shadow-01 rounded-[30px] px-8 py-12 bg-ct2-neutral-01 font-nunito">
       <Image
@@ -36,45 +37,54 @@ const SignIn = () => {
       <p className="bodytext-16px-regular my-4 mx-12 text-center ">
         Xin chào, vui lòng đăng nhập để sử dụng các dịch vụ của ICAN CONNECT.
       </p>
-      <TextInput
-        {...signInForm.getInputProps("email")}
-        required
-        classNames={{
-          input: "rounded-[10px] border-ct2-neutral-02 font-nunito",
-          label: "bodytext-14px-regular text-ct2-neutral-04 mb-2 font-nunito",
-        }}
-        size="md"
-        label="Username / Email đăng nhập"
-        type="email"
-      />
-      <PasswordInput
-        {...signInForm.getInputProps("password")}
-        required
-        className="mt-4"
-        classNames={{
-          input: "rounded-[10px] border-ct2-neutral-02 font-nunito",
-          label: "bodytext-14px-regular text-ct2-neutral-04 mb-2 font-nunito",
-        }}
-        size="md"
-        label="Mật khẩu"
-        visibilityToggleIcon={({ reveal, size }: any) =>
-          reveal ? (
-            <div className="mr-4">
-              <EyeSlash size="24" color="#C4C4C4" />
-            </div>
-          ) : (
-            <div className="mr-4">
-              <Eye size="24" color="#C4C4C4" />
-            </div>
-          )
-        }
-      />
-      <p className="cursor-pointer text-right text-ct2-primary-01 bodytext-button mt-2">
-        Quên mật khẩu
-      </p>
-      <div className="cursor-pointer bodytext-button bg-ct2-primary-01 rounded-lg py-[13px] w-full mt-4 text-center text-ct2-neutral-01">
-        Đăng nhập
-      </div>
+      <form onSubmit={signInForm.onSubmit((value) => handleSubmit(value))}>
+        <TextInput
+          {...signInForm.getInputProps("email")}
+          classNames={{
+            input: "rounded-[10px] font-nunito",
+            label: "bodytext-14px-regular text-ct2-neutral-04 mb-2 font-nunito",
+          }}
+          size="md"
+          label="Username / Email đăng nhập"
+          type="email"
+        />
+        <PasswordInput
+          {...signInForm.getInputProps("password")}
+          className="mt-4"
+          classNames={{
+            input: "rounded-[10px] font-nunito",
+            label: "bodytext-14px-regular text-ct2-neutral-04 mb-2 font-nunito",
+          }}
+          size="md"
+          label="Mật khẩu"
+          visibilityToggleIcon={({ reveal, size }: any) => {
+            return reveal ? (
+              <div className="mr-4">
+                <EyeSlash
+                  size="24"
+                  color={signInForm.errors?.password ? "#E22020" : "#C4C4C4"}
+                />
+              </div>
+            ) : (
+              <div className="mr-4">
+                <Eye
+                  size="24"
+                  color={signInForm.errors?.password ? "#E22020" : "#C4C4C4"}
+                />
+              </div>
+            );
+          }}
+        />
+        <p className="w-fit cursor-pointer ml-auto text-right text-ct2-primary-01 bodytext-button mt-2">
+          Quên mật khẩu
+        </p>
+        <button
+          type="submit"
+          className="cursor-pointer bodytext-button bg-ct2-primary-01 rounded-lg py-[13px] w-full mt-4 text-center text-ct2-neutral-01"
+        >
+          Đăng nhập
+        </button>
+      </form>
       <div className="my-4">
         {/* <hr /> */}
         <Divider
