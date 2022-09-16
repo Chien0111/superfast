17ee7ff42aa2ca6816b197c06caa5bdb6796ec70
nmhung1234 <nmhung1234@hotmail.com>
2022-09-16 08:46:50 +0700

    fix responsive

--- element/solution/Step/index.tsx
+++ element/solution/Step/index.tsx
@@ -4,9 +4,10 @@ import StepMobile from "../../../modecules/Solution/Step/Mobile";
 
 const Step = () => {
   const isMobile = useIsMobile();
+  console.log(isMobile);
   return (
     <div className="bg-[url(/images/solution/bg_solution.png)] bg-cover">
-      {!isMobile ? <StepDesktop /> : <StepMobile />}
+      {isMobile ? <StepMobile /> : <StepDesktop />}
     </div>
   );
 };
--- hooks/useIsMobile.ts
+++ hooks/useIsMobile.ts
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export const useIsMobile = () => {
   const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
     const handleResize = () => {
-      const isMobileCurrent = window.innerWidth <= 984;
+      const isMobileCurrent = window.outerWidth <= 984;
       if (isMobileCurrent !== isMobile) {
         setIsMobile(isMobileCurrent);
       }
     };
     handleResize();
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, [isMobile]);
   return isMobile;
